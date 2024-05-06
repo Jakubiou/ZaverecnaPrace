d15ec4a7960aaf4f706fe7bfef3cbaca76f032ac
Jakubiou <jakubsramek2006@gmail.com>
2024-05-06 18:05:42 +0200

    Added method paintComponent

--- src/ScorePanel.java
+++ src/ScorePanel.java
@@ -11,4 +11,27 @@ public class ScorePanel extends JPanel {
     private int player2Score = 0;
     private Font font = new Font("Arial",Font.BOLD,FONT_SIZE);
 
+    public ScorePanel(){
+        setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
+        setBackground(BACKGROUND_COLOR);
+    }
+
+    public void incrementPlayer1Score(){
+        player1Score++;
+        repaint();
+    }
+    public void incrementPlayer2Score(){
+        player2Score++;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(TEXT_COLOR);
+        g.setFont(font);
+        g.drawString("Score" + player1Score,30,30);
+        g.drawString("Score" + player2Score, PANEL_WIDTH - 200, 30);
+    }
 }
