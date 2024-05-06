0ce91879c68ce4651d7629b84b1a0edbc949c989
Jakubiou <jakubsramek2006@gmail.com>
2024-05-06 17:58:52 +0200

    Added panel and text values

--- src/ScorePanel.java
+++ src/ScorePanel.java
@@ -1,2 +1,14 @@
-public class ScorePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class ScorePanel extends JPanel {
+    private int PANEL_WIDTH = 900;
+    private int PANEL_HEIGHT = 50;
+    private int FONT_SIZE = 20;
+    private Color BACKGROUND_COLOR = Color.DARK_GRAY;
+    private Color TEXT_COLOR = Color.WHITE;
+    private int player1Score = 0;
+    private int player2Score = 0;
+    private Font font = new Font("Arial",Font.BOLD,FONT_SIZE);
+
 }
