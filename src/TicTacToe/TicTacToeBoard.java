ebb2e4dada9394542f8bbd89d863c05ea38547c6
Jakubiou <jakubsramek2006@gmail.com>
2024-05-08 22:35:49 +0200

    Added constructor and JButton

--- src/TicTacToe/TicTacToeBoard.java
+++ src/TicTacToe/TicTacToeBoard.java
@@ -1,4 +1,15 @@
 package TicTacToe;
 
-public class TicTacToeBoard {
+import javax.swing.*;
+import java.awt.*;
+
+public class TicTacToeBoard extends JPanel {
+    private JButton[][] buttons;
+    private char currentPlayer;
+
+    public TicTacToeBoard(){
+        setLayout(new GridLayout(3,3,10,10));
+        setBackground(Color.WHITE);
+        setPreferredSize(new Dimension(300,300));
+    }
 }
