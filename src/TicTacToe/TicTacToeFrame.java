89ca143441703347f983403398a023bcbac8c7f1
Jakubiou <jakubsramek2006@gmail.com>
2024-05-09 20:53:56 +0200

    Added constructor

--- src/TicTacToe/TicTacToeFrame.java
+++ src/TicTacToe/TicTacToeFrame.java
@@ -1,7 +1,14 @@
 package TicTacToe;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TicTacToeFrame extends JFrame {
     public TicTacToeBoard board;
+
+    public TicTacToeFrame(){
+        board = new TicTacToeBoard();
+        setLayout(new BorderLayout());
+        add(board,BorderLayout.CENTER);
+    }
 }
