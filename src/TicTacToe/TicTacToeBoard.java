16d82ab8a0abe182eade9892bdd9eeede09f4ad6
Jakubiou <jakubsramek2006@gmail.com>
2024-05-09 20:14:55 +0200

    Added method initializeBoard

--- src/TicTacToe/TicTacToeBoard.java
+++ src/TicTacToe/TicTacToeBoard.java
@@ -2,6 +2,8 @@ package TicTacToe;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TicTacToeBoard extends JPanel {
     private JButton[][] buttons;
@@ -11,5 +13,22 @@ public class TicTacToeBoard extends JPanel {
         setLayout(new GridLayout(3,3,10,10));
         setBackground(Color.WHITE);
         setPreferredSize(new Dimension(300,300));
+        buttons = new JButton[3][3];
+        currentPlayer = 'X';
+    }
+    public void initializeBoard(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                JButton button = new JButton();
+                button.setFont(new Font("Arial",Font.BOLD,40));
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        int row = -1;
+                        int col = -1;
+                    }
+                });
+            }
+        }
     }
 }
