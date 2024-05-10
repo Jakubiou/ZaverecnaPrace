9c41e333e508533d2e6bb7898cf367f0054edd42
Jakubiou <jakubsramek2006@gmail.com>
2024-05-10 18:14:09 +0200

    Added pongButton in to GameMenu

--- src/GameMenu.java
+++ src/GameMenu.java
@@ -1,5 +1,9 @@
+import Pong.StartPong;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameMenu extends JFrame {
 
@@ -11,5 +15,17 @@ public class GameMenu extends JFrame {
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3,1,10,10));
         panel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        add(panel);
+        setVisible(true);
+        JButton pongButton = new JButton("Play Pong");
+        pongButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StartPong startPong = new StartPong();
+                startPong.startPong();
+                setVisible(false);
+            }
+        });
+        panel.add(pongButton);
     }
 }
