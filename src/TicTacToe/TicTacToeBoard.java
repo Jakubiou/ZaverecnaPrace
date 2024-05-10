7336447c829943bd85bd10951b880c179c90cef9
Jakubiou <jakubsramek2006@gmail.com>
2024-05-10 20:42:07 +0200

    Updated method checkWinner (added onGameEnd)

--- src/TicTacToe/TicTacToeBoard.java
+++ src/TicTacToe/TicTacToeBoard.java
@@ -8,8 +8,8 @@ import java.awt.event.ActionListener;
 public class TicTacToeBoard extends JPanel {
     private JButton[][] buttons;
     private char currentPlayer;
-
     private boolean gameEnded;
+    private GameEndListener gameEndListener;
 
     public TicTacToeBoard(){
         setLayout(new GridLayout(3,3,10,10));
@@ -57,18 +57,22 @@ public class TicTacToeBoard extends JPanel {
         for(int i = 0; i < 3; i++){
             if (buttons[i][0].getText().equals(buttons[i][1].getText()) && buttons[i][0].getText().equals(buttons[i][2].getText()) && !buttons[i][0].getText().isEmpty()) {
                 gameEnded = true;
+                gameEndListener.onGameEnd(currentPlayer);
             }
         }
         for(int i = 0; i < 3; i++) {
             if (buttons[0][i].getText().equals(buttons[1][i].getText()) && buttons[0][i].getText().equals(buttons[2][i].getText()) && !buttons[0][i].getText().isEmpty()) {
                 gameEnded = true;
+                gameEndListener.onGameEnd(currentPlayer);
             }
         }
         if (buttons[0][0].getText().equals(buttons[1][1].getText()) && buttons[0][0].getText().equals(buttons[2][2].getText()) && !buttons[0][0].getText().isEmpty()) {
             gameEnded = true;
+            gameEndListener.onGameEnd(currentPlayer);
         }
         if (buttons[0][2].getText().equals(buttons[1][1].getText()) && buttons[0][2].getText().equals(buttons[2][0].getText()) && !buttons[0][2].getText().isEmpty()) {
             gameEnded = true;
+            gameEndListener.onGameEnd(currentPlayer);
         }
     }
 }
