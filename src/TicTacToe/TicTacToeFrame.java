e50d369260b7402b872a4fb1ede26a5e10a1540f
Jakubiou <jakubsramek2006@gmail.com>
2024-05-12 19:49:18 +0200

    Added method updateStatus and handleGameEnd

--- src/TicTacToe/TicTacToeFrame.java
+++ src/TicTacToe/TicTacToeFrame.java
@@ -4,11 +4,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TicTacToeFrame extends JFrame {
-    public TicTacToeBoard board;
+    private TicTacToeBoard board;
+    private JLabel statusLabel;
+
 
     public TicTacToeFrame(){
         board = new TicTacToeBoard();
         setLayout(new BorderLayout());
         add(board,BorderLayout.CENTER);
+    }
+
+    public void updateStatus(String message){
+        statusLabel.setText(message);
+    }
+
+    public void handleGameEnd(char winner){
+        if(winner == ' '){
+            updateStatus("It's a tie!");
+        }else {
+            updateStatus("Player " + winner + " wins!");
+        }
     }
 }
