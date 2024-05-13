de7bddaa7fecc48e78568ba29e8a4477746d89e9
Jakubiou <jakubsramek2006@gmail.com>
2024-05-13 19:33:47 +0200

    Added parameters for blocks

--- src/TowerBulder/TowerBuilder.java
+++ src/TowerBulder/TowerBuilder.java
@@ -6,4 +6,13 @@ public class TowerBuilder extends JPanel{
 
     private int PANEL_WIDTH = 600;
     private int PANEL_HEIGHT = 750;
+    private int BASE_BLOCK_wIDTH = 200;
+    private int BLOCK_HEIGHT = 40;
+    private int baseBlockX;
+    private int baseBlockY;
+    private int movingBlockX;
+    private int movingBlockY;
+    private boolean isMovingRight = true;
+    private boolean isSpacePressed = false;
+    private  boolean gameOver = false;
 }
