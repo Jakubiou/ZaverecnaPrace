cbb5fdad395ee260cc48c2c76508ac7dff784c3c
Jakubiou <jakubsramek2006@gmail.com>
2024-05-13 19:45:51 +0200

    Added Block constructor and random colors of blocks

--- src/TowerBulder/Block.java
+++ src/TowerBulder/Block.java
@@ -1,4 +1,18 @@
 package TowerBulder;
 
-public class Block {
+import java.awt.*;
+import java.util.Random;
+
+public class Block extends Rectangle {
+
+    private Color color;
+
+    public Block(int x, int y, int width,int height){
+        super(x,y,width,height);
+        Random random = new Random();
+        this.color = new Color(random.nextInt(256),random.nextInt(256),random.nextInt(256));
+    }
+    public Color getColor(){
+        return color;
+    }
 }
