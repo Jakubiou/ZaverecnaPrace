2291303e0e43a995f6221b4b3c82fdd2cfc0e232
Jakubiou <jakubsramek2006@gmail.com>
2024-05-13 16:37:32 +0200

    Increased size of tic tac toe

--- src/TicTacToe/TicTacToeBoard.java
+++ src/TicTacToe/TicTacToeBoard.java
@@ -15,9 +15,9 @@ public class TicTacToeBoard extends JPanel {
 
     public TicTacToeBoard(GameEndListener gameEndListener){
         this.gameEndListener = gameEndListener;
-        setLayout(new GridLayout(3,3,10,10));
+        setLayout(new GridLayout(3,3,20,20));
         setBackground(Color.WHITE);
-        setPreferredSize(new Dimension(300,300));
+        setPreferredSize(new Dimension(600,600));
         buttons = new JButton[3][3];
         currentPlayer = 'X';
         gameEnded = false;
@@ -27,7 +27,7 @@ public class TicTacToeBoard extends JPanel {
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
                 JButton button = new JButton();
-                button.setFont(new Font("Arial",Font.BOLD,40));
+                button.setFont(new Font("Arial",Font.BOLD,100));
                 button.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
@@ -64,21 +64,25 @@ public class TicTacToeBoard extends JPanel {
             if (buttons[i][0].getText().equals(buttons[i][1].getText()) && buttons[i][0].getText().equals(buttons[i][2].getText()) && !buttons[i][0].getText().isEmpty()) {
                 gameEnded = true;
                 gameEndListener.onGameEnd(currentPlayer);
+                return;
             }
         }
         for (int i = 0; i < 3; i++) {
             if (buttons[0][i].getText().equals(buttons[1][i].getText()) && buttons[0][i].getText().equals(buttons[2][i].getText()) && !buttons[0][i].getText().isEmpty()) {
                 gameEnded = true;
                 gameEndListener.onGameEnd(currentPlayer);
+                return;
             }
         }
         if (buttons[0][0].getText().equals(buttons[1][1].getText()) && buttons[0][0].getText().equals(buttons[2][2].getText()) && !buttons[0][0].getText().isEmpty()) {
             gameEnded = true;
             gameEndListener.onGameEnd(currentPlayer);
+            return;
         }
         if (buttons[0][2].getText().equals(buttons[1][1].getText()) && buttons[0][2].getText().equals(buttons[2][0].getText()) && !buttons[0][2].getText().isEmpty()) {
             gameEnded = true;
             gameEndListener.onGameEnd(currentPlayer);
+            return;
         }
         if(gameEnded){
             return;
