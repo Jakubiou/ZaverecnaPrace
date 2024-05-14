f371e2110b7cb47ae098be4a77e3ce2c222a5ed8
Jakubiou <jakubsramek2006@gmail.com>
2024-05-14 21:15:49 +0200

    Added constructor

--- src/TowerBulder/DifficultyMenu.java
+++ src/TowerBulder/DifficultyMenu.java
@@ -1,4 +1,16 @@
 package TowerBulder;
 
-public class DifficultyMenu {
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class DifficultyMenu extends JMenu {
+
+    private JRadioButtonMenuItem easyMenu;
+    private JRadioButtonMenuItem normalMenu;
+    private JRadioButtonMenuItem hardMenu;
+    private ButtonGroup difficultyGroup;
+
+    public DifficultyMenu(ActionListener listener){
+        super("Difficulty");
+    }
 }
