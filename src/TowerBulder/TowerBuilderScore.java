dca21218d18db8b03954cf7672ae54f4f45f7e97
Jakubiou <jakubsramek2006@gmail.com>
2024-05-14 21:34:08 +0200

    Added method increaseScore and constructor

--- src/TowerBulder/TowerBuilderScore.java
+++ src/TowerBulder/TowerBuilderScore.java
@@ -1,4 +1,18 @@
 package TowerBulder;
 
 public class TowerBuilderScore {
+
+    private int score;
+
+    public TowerBuilderScore(){
+        this.score = 0;
+    }
+
+    public void increaseScore(){
+        score++;
+    }
+
+    public int getScore(){
+        return score;
+    }
 }
