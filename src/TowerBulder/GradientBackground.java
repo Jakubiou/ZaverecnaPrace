a3a72ae1a0cc75b7baf614c55acc3427c7cc8b95
Jakubiou <jakubsramek2006@gmail.com>
2024-05-19 15:48:37 +0200

    Added method drawBackground and constructor

--- src/TowerBulder/GradientBackground.java
+++ src/TowerBulder/GradientBackground.java
@@ -1,4 +1,21 @@
 package TowerBulder;
 
+import java.awt.*;
+
 public class GradientBackground {
+
+    private Color startColor;
+    private Color endColor;
+
+    public GradientBackground(Color startColor, Color endColor) {
+        this.startColor = startColor;
+        this.endColor = endColor;
+    }
+
+    public void drawBackground(Graphics g, int width, int height){
+        Graphics2D graphics2D = (Graphics2D) g;
+        GradientPaint gradientPaint = new GradientPaint(0,0,startColor,0,height,endColor);
+        graphics2D.setPaint(gradientPaint);
+        graphics2D.fillRect(0,0,width,height);
+    }
 }
