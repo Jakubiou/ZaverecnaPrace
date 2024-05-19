79acbb5ffd25f83e4b30b3ecbe286f94efd79a48
Jakubiou <jakubsramek2006@gmail.com>
2024-05-19 13:57:28 +0200

    Added class Buttons and moved in to class Buttons method createButton

---
+++ src/TicTacToe/Buttons.java
@@ -0,0 +1,33 @@
+package TicTacToe;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Buttons {
+
+    public Buttons() {
+    }
+
+    protected JButton createMenuButton(String text,int width,int height) {
+        JButton button = new JButton(text);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setFont(new Font("Arial", Font.BOLD, 24));
+        button.setFocusPainted(false);
+        button.setBackground(new Color(255, 255, 255));
+        button.setForeground(new Color(0, 85, 170));
+        button.setPreferredSize(new Dimension(width, height));
+        button.setMaximumSize(new Dimension(width, height));
+
+        button.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt){
+                button.setBackground(new Color(0,85,170));
+                button.setForeground(new Color(255,255,255));
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt){
+                button.setBackground(new Color(255,255,255));
+                button.setForeground(new Color(0,85,170));
+            }
+        });
+        return button;
+    }
+}
