fca22bdae4dfee2469099ed01bdd29a7b0285113
Jakubiou <jakubsramek2006@gmail.com>
2024-05-23 08:33:02 +0200

    Added ArrayList highScore and added method addHighScore

--- src/TowerBuilder/HighScoreManager.java
+++ src/TowerBuilder/HighScoreManager.java
@@ -1,4 +1,16 @@
 package TowerBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HighScoreManager {
+
+    private List<HighScore> highScores = new ArrayList<>();
+
+    public void addHighScore(String name,int score){
+        highScores.add(new HighScore(name,score));
+        if(highScores.size() > 3){
+            highScores.remove(3);
+        }
+    }
 }
