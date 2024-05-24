0a6036e9e734c810467ae77a7009347a70b28545
Jakubiou <jakubsramek2006@gmail.com>
2024-05-24 16:17:55 +0200

    Added Java documentation

--- src/Pong/ScorePanel.java
+++ src/Pong/ScorePanel.java
@@ -3,6 +3,10 @@ package Pong;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * The ScorePanel class represents a panel displaying player scores.
+ * Extends the JPanel class to provide player score rendering.
+ */
 public class ScorePanel extends JPanel {
     private int PANEL_WIDTH = 900;
     private int PANEL_HEIGHT = 50;
@@ -13,20 +17,35 @@ public class ScorePanel extends JPanel {
     private int player2Score = 0;
     private Font font = new Font("Arial",Font.BOLD,FONT_SIZE);
 
+    /**
+     * Constructor for the ScorePanel class.
+     * Sets the dimensions and background color of the panel.
+     */
     public ScorePanel(){
         setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
         setBackground(BACKGROUND_COLOR);
     }
 
+    /**
+     * Increases player 1's score by 1 point and redraws the panel.
+     */
     public void incrementPlayer1Score(){
         player1Score++;
         repaint();
     }
+
+    /**
+     * Increases player 2's score by 1 point and redraws the panel.
+     */
     public void incrementPlayer2Score(){
         player2Score++;
         repaint();
     }
 
+    /**
+     * Plots player scores on a panel.
+     * @param g the <code>Graphics</code> object to protect
+     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
