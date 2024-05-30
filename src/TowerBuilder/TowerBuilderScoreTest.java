f94a79c22ebe42fa0983de7ba3aded44b7102f94
Jakubiou <jakubsramek2006@gmail.com>
2024-05-30 16:08:09 +0200

    Added class TowerBuilderScoreTest

---
+++ src/TowerBuilder/TowerBuilderScoreTest.java
@@ -0,0 +1,13 @@
+package TowerBuilder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TowerBuilderScoreTest {
+
+    @Test
+    void increaseScore() {
+
+    }
+}
