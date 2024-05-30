c71588dc9b46c367029c5556a7b9cf08d5694f2d
Jakubiou <jakubsramek2006@gmail.com>
2024-05-30 16:07:57 +0200

    Added class TicTacToeBoardTest

---
+++ src/TicTacToe/TicTacToeBoardTest.java
@@ -0,0 +1,13 @@
+package TicTacToe;
+
+
+class TicTacToeBoardTest {
+
+    @org.junit.jupiter.api.Test
+    void checkWinner() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void switchPlayer() {
+    }
+}
