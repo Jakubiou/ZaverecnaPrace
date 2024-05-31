e8a5f5972b53482a8a1ebd1019a8d8457bd7e5ba
Jakubiou <jakubsramek2006@gmail.com>
2024-05-31 16:21:14 +0200

    Added addHighScore test

--- src/TowerBuilder/HighScoreManagerTest.java
+++ src/TowerBuilder/HighScoreManagerTest.java
@@ -1,13 +1,28 @@
 package TowerBuilder;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HighScoreManagerTest {
 
+    @BeforeEach
+    void setUp() {
+        HighScoreManager.getHighScores();
+    }
+
     @Test
     void addHighScore() {
+        HighScoreManager.addHighScore("Player1",50);
+        HighScoreManager.addHighScore("Player2",40);
+        HighScoreManager.addHighScore("Player3",30);
+        List<HighScore> highScores = HighScoreManager.getHighScores();
+        assertEquals(3,highScores.size());
+        assertEquals("Player1",highScores.get(0).getName());
+        assertEquals(50,highScores.get(0).getScore());
     }
 
     @Test
